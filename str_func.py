c4bfc74f6ab8c33beced53aa4d1d81e8580f25d1
fomihyh-vitaliy <fomihyh-vitaliy@mail.ru>
2024-06-12 14:18:53 +0300

    Added second function with docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def to_uppercase(string):
+    """                                                                                                               Функция принимает строку и возвращает ее со всеми заглавными буквами.
+    """
     return string.upper()
+
+
+def capitalize_words(s):
+    """
+    Функция принимает строку и делает заглавными первые буквы каждого слова в этой строке
+    """
+    return ' '.join(word.capitalize() for word in s.split())
